1d14478db1a90b1189f0312b83d28fd801fa9184
seonhongmun <mshg_@naver.com>
2025-04-09 17:46:23 +0900

    ✨add user, resume, resumeeducation

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,3 +2,5 @@ from .base import Base
 from .users import User
 from .company_users import CompanyUser
 from .company_info import CompanyInfo
+from .resumes import Resume
+from .resumes_educations import ResumeEducation
