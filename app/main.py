2245e4266f8a078cf95cd8b09fe9e3874b89f9f7
seonhongmun <mshg_@naver.com>
2025-04-13 05:19:44 +0900

    🎨 user models 수정(관심분야 이슈)

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
-from app.domains.users.router import router as users_router
-from app.domains.job_postings.router import router as job_postings_router
-from app.core.config import ENVIRONMENT
-from fastapi import FastAPI, Security
 from fastapi.openapi.utils import get_openapi
-from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from fastapi.security import HTTPBearer
+
 from app.admin.admin import setup_admin
+from app.core.config import ENVIRONMENT
+from app.domains.favorites.router import router as favorites_router
+from app.domains.job_postings.router import router as job_postings_router
+from app.domains.users.oauth.social_router import router as social_router
+from app.domains.users.router import router as users_router
 
 # FastAPI 애플리케이션 인스턴스 생성 (프로젝트 제목 및 버전 설정)
 app = FastAPI(title="My FastAPI Project", version="0.1.0")
@@ -13,6 +15,7 @@ app = FastAPI(title="My FastAPI Project", version="0.1.0")
 setup_admin(app)
 
 bearer_scheme = HTTPBearer()
+
 
 def custom_openapi():
     if app.openapi_schema:
@@ -37,13 +40,16 @@ def custom_openapi():
     app.openapi_schema = openapi_schema
     return app.openapi_schema
 
+
 app.openapi = custom_openapi
+
+
 @app.get("/")
 async def root():
     return {"message": f"Hello World in {ENVIRONMENT} environment"}
 
-# 사용자 도메인 라우터를 애플리케이션에 포함
-# prefix: 해당 라우터의 모든 경로 앞에 "/api/users" 추가
-# tags: API 문서에서 해당 라우터의 경로들을 "users" 그룹으로 묶음
+
 app.include_router(users_router)
 app.include_router(job_postings_router)
+app.include_router(favorites_router)
+app.include_router(social_router)
