dbf32d534cd78e264db9ee9a69d5c2b35f50489b
shinezeee <h46998600@gmail.com>
2025-04-17 04:44:00 +0900

    ⚡️ 응답스키마 추가

--- app/domains/company_users/schemas.py
+++ app/domains/company_users/schemas.py
@@ -60,3 +60,71 @@ class ResetCompanyUserPassword(FindCompanyUserEmail):
     email: EmailStr
     new_password: str
     confirm_password: str
+
+
+### 사업자등록번호 유효성 확인 요청
+class BRNValidationRequest(BaseModel):
+    business_reg_number: str
+    opening_date: str  # YYYYMMDD
+    ceo_name: str
+
+
+### 성공 응답 스키마(Generic)
+T = TypeVar("T")
+
+
+class SuccessResponse(GenericModel, Generic[T]):
+    status: str = "success"
+    message: str
+    data: Optional[T] = None
+
+
+### 응답 스키마
+class CompanyInfoResponse(BaseModel):  # 기업 정보
+    company_name: str
+    company_intro: str
+    business_reg_number: str
+    opening_date: date
+    ceo_name: str
+
+    class Config:
+        orm_mode = True
+
+
+class JobPostingsSummary(BaseModel):  # 공고 요약
+    id: int
+    title: str
+    work_address: str
+    deadline_at: date
+    is_always_recruiting: bool
+
+    class Config:
+        orm_mode = True
+
+
+class CompanyUserResponse(BaseModel):  # 기업 유저 정보
+    company_user_id: int
+    email: EmailStr
+    manager_name: str
+    manager_email: EmailStr
+    manager_phone: str
+    company: CompanyInfoResponse
+
+    class Config:
+        orm_mode = True
+
+
+class CompanyUserLoginResponse(BaseModel):  # 로그인 응답
+    company_user_id: int
+    email: EmailStr
+    company_name: str
+    token_type: str = "bearer"
+    access_token: str
+    refresh_token: str
+
+    class Config:
+        orm_mode = True
+
+
+class CompanyUserInfo(CompanyUserResponse):
+    job_postings: List[JobPostingsSummary] = Field(default_factory=list)
