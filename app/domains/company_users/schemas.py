d02b30a254f994b115ca39e89fcbf2d143d85829
shinezeee <h46998600@gmail.com>
2025-04-17 04:43:28 +0900

    ♻️ 스키마 리팩토링

--- app/domains/company_users/schemas.py
+++ app/domains/company_users/schemas.py
@@ -1,58 +1,61 @@
 from datetime import date
-from typing import Optional
+from typing import Generic, Optional, TypeVar
 
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field
+from pydantic.generics import GenericModel
+from typing_extensions import List
 
 
-# 기업 회원 가입 - 요청 바디용
-class CompanyUserRequest(BaseModel):
+### 기업 회원 공통 베이스
+class CompanyUserBase(BaseModel):
     email: EmailStr  # 로그인 이메일
-    password: str  # 비밀 번호
-
-    # 기업 정보
+    manager_name: str  # 담당자 이름
+    manager_phone: str  # 담당자 전화 번호
+    manager_email: EmailStr  # 담당자 이메일
     company_name: str  # 기업명
     ceo_name: str  # 대표자 성함
     opening_date: date  # 개업 날짜 (YYYY-MM-DD)
     business_reg_number: str  # 사업자등록번호
     company_intro: str  # 기업 소개
 
-    # 담당자
-    manager_name: str  # 담당자 이름
-    manager_phone: str  # 담당자 전화 번호
-    manager_email: EmailStr  # 담당자 이메일
+
+### 패스워드 확인용 믹스인
+class PasswordMixin(BaseModel):
+    password: str
+    confirm_password: str
 
 
-# 사업자등록번호 유효성 확인
-class BRNValidationRequest(BaseModel):
-    business_reg_number: str
-    opening_date: str  # YYYYMMDD
-    ceo_name: str
+### 기업 화원 가입 요청
+class CompanyUserRegisterRequest(CompanyUserBase, PasswordMixin):
+    pass
 
 
-# 기업 회원 로그인
+### 기업 회원 로그인 요청
 class CompanyUserLoginRequest(BaseModel):
     email: EmailStr
     password: str
 
 
-# 기업 회원 수정
+### 기업 회원 수정 요청 (선택필드)
 class CompanyUserUpdateRequest(BaseModel):
-    company_intro: Optional[str]
-    address: Optional[str]
-    company_image: Optional[str]
-    manager_name: Optional[str]
-    manager_phone: Optional[str]
-    manager_email: Optional[str]
+    manager_name: Optional[str] = None
+    manager_phone: Optional[str] = None
+    manager_email: Optional[EmailStr] = None
+    password: Optional[str] = None
+    confirm_password: Optional[str] = None
+    company_intro: Optional[str] = None
+    address: Optional[str] = None
+    company_image: Optional[str] = None
 
 
-# 아이디 찾기
+### 아이디 찾기 요청
 class FindCompanyUserEmail(BaseModel):
     business_reg_number: str
     opening_date: date
     ceo_name: str
 
 
-# 비밀번호 재설정
+### 비밀번호 재설정 요청
 class ResetCompanyUserPassword(FindCompanyUserEmail):
     email: EmailStr
     new_password: str
